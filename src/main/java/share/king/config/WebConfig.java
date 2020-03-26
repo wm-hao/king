c9445d3d0ddf7c63b192eeda121c2b20e6b22b28
zhuhhl <zhuhh@asiainfo.com>
2020-03-26 22:55:46 +0800

    解决拦截器跨域

--- src/main/java/share/king/config/WebConfig.java
+++ src/main/java/share/king/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import share.king.interceptor.HeaderInterceptor;
 import share.king.interceptor.TokenInterceptor;
 
 @Configuration
@@ -14,7 +15,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HeaderInterceptor()).addPathPatterns("/**");
         registry.addInterceptor(tokenInterceptor).addPathPatterns("/**")
                 .excludePathPatterns("/user/validate", "/user/exists");
+
     }
 }
